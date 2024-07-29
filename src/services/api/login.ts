c2cc7e219b907064ebe25d9b2000be5eeeb2dfbe
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2024-07-30 03:43:16 +0700

    refactor

--- src/services/api/login.ts
+++ src/services/api/login.ts
@@ -9,9 +9,7 @@ type loginResponse = {
     accessToken: string;
 }
 
-export const login = ({ mobileNumber, password }: loginRequest): Promise<loginResponse> =>
-    apiInstance.post('/auth/user/v3/login', {
-        mobileNumber, password
-    }).then(res => res.data)
+export const login = (req: loginRequest): Promise<loginResponse> =>
+    apiInstance.post('/auth/user/v3/login', req).then(res => res.data)
 
 export type loginApi = typeof login;
